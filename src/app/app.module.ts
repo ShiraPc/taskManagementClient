6e315c9506f14d15ee45c1d84a763ea20fca4781
tamarBroner <tb0533153976@gmail.com>
2022-05-24 12:29:46 +0300

    t

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignInComponent } from './moduls/login-module/sign-in/sign-in.component';
 import { EntryComponent } from './moduls/login-module/entry/entry.component';
+import { DailyComponent } from './moduls/calendar-module/daily/daily.component';
+import { WeeklyComponent } from './moduls/calendar-module/weekly/weekly.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignInComponent,
-    EntryComponent
+    EntryComponent,
+    DailyComponent,
+    WeeklyComponent
   ],
   imports: [
     BrowserModule,
