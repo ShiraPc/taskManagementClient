fbefb944f500a790fb9a25d45cf2a68df122af1d
tamarBroner <tb0533153976@gmail.com>
2022-05-24 12:38:56 +0300

    t

--- src/app/moduls/login-module/sign-in/sign-in.component.ts
+++ src/app/moduls/login-module/sign-in/sign-in.component.ts
@@ -7,8 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignInComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    
+   }
+  signIn(){
 
+  }
   ngOnInit(): void {
   }
 
