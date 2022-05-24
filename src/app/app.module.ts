12f41f0376f07b609f25a50b0ea63edc7fe81811
tamarBroner <tb0533153976@gmail.com>
2022-05-24 12:27:19 +0300

    t

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignInComponent } from './moduls/login-module/sign-in/sign-in.component';
+import { EntryComponent } from './moduls/login-module/entry/entry.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
+    EntryComponent
   ],
   imports: [
     BrowserModule,
